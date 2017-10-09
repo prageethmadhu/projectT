fc2ce5b87274268eae10a4bcee2fb3fdab4da001
admin <prageeth.madhu@gmail.com>
2017-10-09 11:57:57 +0530

    news added with formatting

--- src/app/mock-news.ts
+++ src/app/mock-news.ts
@@ -1,14 +1,13 @@
 import { News } from './news';
 
 export const NewsList: News[] = [
-  { id: 1, name: 'Mr. Nice',path:"c4.jpg",discription:"this is the news" },
-  { id: 2, name: 'Narco' ,path:"c4.jpg",discription:"this is the news"},
-  { id: 3, name: 'Bombasto' ,path:"c4.jpg",discription:"this is the news"},
-  { id: 4, name: 'Celeritas' ,path:"c4.jpg",discription:"this is the news"},
-  { id: 5, name: 'Magneta' ,path:"c4.jpg",discription:"this is the news"},
-  { id: 6, name: 'RubberMan' ,path:"c4.jpg",discription:"this is the news"},
-  { id: 7, name: 'Dynama' ,path:"c4.jpg",discription:"this is the news"},
-  { id: 8, name: 'Dr IQ' ,path:"c4.jpg",discription:"this is the news"},
-  { id: 9, name: 'Magma' ,path:"c4.jpg",discription:"this is the news"},
+  { id: 1, name: 'Mr. Nice',path:"c1.jpg",discription:"Biodiversity, a portmanteau of bio (life) and diversity, generally refers to the variety and variability of life on Earth. According to the United Nations Environment Programme, biodiversity typically measures variation at the genetic, the species, and the ecosystem level.[1] Terrestrial biodiversity tends to be greater near the equator,[2] which seems to be the result of the warm climate and high primary productivity.[3] Biodiversity is not distributed evenly on Earth, and is richest in the tropics. These tropical forest ecosystems cover less than 10 per cent of earth's surface, and contain about 90 percent of the world's species" },
+  { id: 2, name: 'Narco' ,path:"c2.jpg",discription:"Botany originated as herbalism, the study and use of plants for their medicinal properties.[6] Many records of the Holocene period date early botanical knowledge as far back as 10,000 years ago.[7] This early unrecorded knowledge of plants was discovered in ancient sites of human occupation within Tennessee, which make up much of the Cherokee land today.[7] The early recorded history of botany includes many ancient writings and plant classifications. Examples of early botanical works have been found in ancient texts from India dating back to before 1100 BC,[8][9] in archaic Avestan writings, and in works from China before it was unified in 221 BC.[8][10]"},
+  { id: 3, name: 'Bombasto' ,path:"c3.jpg",discription:"සමපෝෂ වල තියෙන්නේ මුංඇට, කුරක්කන්, තිරිගු පිටි වගේ ඒව. මේ ඇට වර්ග ජාන වෙනස් කරල හදන ඒව නෙවෙයි නම් අව්ලක් නෑ.... තව මේවට කලවම් කරන දේවල් මොනවද කියල දන්නේ නැනේ."},
+  { id: 4, name: 'Celeritas' ,path:"c4.jpg",discription:"I'm coding a Blackjack program, and I am trying to setup a MouseListener for a label I have, but I am unable to get it to work. I have looked at many websites and other people's question"},
+  { id: 5, name: 'Magneta' ,path:"c5.jpg",discription:"The Esala Festival is held annually in Kandy, the hill-capital, during the full moon month of Esala which falls in July/August. Held for the la"},
+  { id: 6, name: 'RubberMan' ,path:"download.png",discription:"this is the news6"},
+  { id: 7, name: 'Dynama' ,path:"mp2.jpg",discription:"this is the news7"},
+  { id: 8, name: 'Dr IQ' ,path:"mpl1.jpg",discription:"this is the news8"},
   
 ];
